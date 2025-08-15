d4abcac04f42d39c7d5ec14717b323a84b8e0e17
Christopher Chau <christopherchau@ucsb.edu>
2025-08-15 16:37:38 -0700

    Removed "other" option from dropdowns

--- src/components/input_card/roles.js
+++ src/components/input_card/roles.js
@@ -1,9 +1,9 @@
-export const behavioralRoles = ["Software Engineering", "Product Management", "UX Design", "IS Security" , "Data Science", "Other"]
+export const behavioralRoles = ["Software Engineering", "Product Management", "UX Design", "IS Security" , "Data Science"]
 export const behavioralFocus = ["Conflict resolution", "Leadership", "Teamwork", "Time management", "Adaptability", "Decision making", "Communication", "Random"]
 
-export const technicalRoles = ["Full-stack Engineer", "Frontend Engineer", "Backend Engineer", "DevOps Engineer" , "AI/ML Engineer", "Mobile Developer", "Data Engineer", "Other"]
+export const technicalRoles = ["Full-stack Engineer", "Frontend Engineer", "Backend Engineer", "DevOps Engineer" , "AI/ML Engineer", "Mobile Developer", "Data Engineer"]
 
-export const experienceLevels = ["Intern", "Entry-Level", "Junior / Mid-Level", "Senior", "Staff/Principal", "Other"]
+export const experienceLevels = ["Intern", "Entry-Level", "Junior / Mid-Level", "Senior", "Staff/Principal"]
 
 export const DROPDOWN_CONFIGS = {
   behavioral: [
